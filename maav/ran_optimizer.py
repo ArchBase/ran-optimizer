e0e884809341dcdbd51e9cf17a055ac3ca961614
ArchBase <archbase557@gmail.com>
2024-04-18 11:34:47 +0530

    function wise debugging compete
    checked functions include:
    apply_grad()
    calc_grad()
    get_params_array()
    get_random_grad()
    ......

--- maav/ran_optimizer.py
+++ maav/ran_optimizer.py
@@ -2,6 +2,7 @@ import numpy as np
 from maav.configuration import config
 import maav.configuration as configuration
 import tensorflow as tf
+import copy
 
 class Weight_Manipulator:
     def __init__(self) -> None:
@@ -10,18 +11,25 @@ class Weight_Manipulator:
         self.line_params = []
         self.grad = []
         self.grad_array_index = 0
+    def generate_random_grad(self):
+        self.grad = []
+        for _ in range(self.weights_count):
+            self.grad.append(np.random.uniform(-1, 1))
+        return self.grad.copy()
     def _format_params(self, params):
-        self.weights_count += 1
+        
         if isinstance(params, np.ndarray) or type(params) is list:
             for i in range(len(params)):
                 params[i] = self._format_params(params[i])
             return params
         else:
+            self.weights_count += 1
             self.line_params.append(params)
             return params
     
     def get_params_array(self, params):
         self.weights_count = 0
+        self.line_params = []
         self.shape_reference = params
         self._format_params(params)
         return self.line_params
@@ -44,20 +52,39 @@ class Weight_Manipulator:
     def _apply_grad_to_weights(self, params):
         if isinstance(params, np.ndarray) or type(params) is list:
             for i in range(len(params)):
-                params[i] = self._format_params(params[i])
+                params[i] = self._apply_grad_to_weights(params[i])
             return params
         else:
             params += self.grad[self.grad_array_index]
             self.grad_array_index += 1
+            #print("updating")
             return params
     
-    def apply_grad(self, grad, new_params):
+    def apply_grad(self, grad, params):
         self.grad_array_index = 0
         self.grad = grad
         if len(grad) != self.weights_count:
             print("Shape error")
         else:
-            return self._apply_grad_to_weights(new_params)
+            return self._apply_grad_to_weights(params)
+    def calculate_gradient(self, prev_params=[], new_params=[], negate=False):
+        #print("Params: ")
+        #print(prev_params)
+        #print(new_params)
+        prev = self.get_params_array(prev_params)
+        new = self.get_params_array(new_params)
+
+        self.grad = []
+
+        if len(prev) != len(new):
+            print("Shape error")
+        else:
+            for index in range(len(prev)):
+                #print("updating " + str(new[index]) + " and " + str(prev[index]))
+                self.grad.append(new[index] - prev[index])
+                if negate:
+                    self.grad[-1] = -self.grad[-1]
+            return self.grad.copy()
 
 class optimizer:
     def __init__(self, model, x_train, y_train) -> None:
@@ -72,28 +99,10 @@ class optimizer:
         self.prev_loss = None
         self.new_params = None
         self.new_loss = None
+        self.epoch_count = 0
+        self.epochs = 0
     
-    def calculate_gradient(self, prev=[], new=[], negate=False):
-        prev = self.weight_maniputlator.get_params_array(prev)
-        new = self.weight_maniputlator.get_params_array(new)
-
-        grad = []
-
-        if len(prev) != len(new):
-            print("Shape error")
-        else:
-            for index in range(len(prev)):
-                grad.append(new[index] - prev[index])
-                if negate:
-                    grad[-1] = -grad[-1]
-        return grad
     
-
-    def apply_grad(self, grad=[]):
-        if len(grad) != self.weight_maniputlator.weights_count:
-            print("Shape error")
-        else:
-            pass
 
     def get_loss(self):
         x_test_tensor = tf.convert_to_tensor(self.x_train, dtype=tf.int32)
@@ -101,9 +110,65 @@ class optimizer:
         something = self.model.evaluate(x_test_tensor, y_test_tensor)
         return something
 
-    def step_update(self):
-        
+    def step_update(self, grad):
+        if self.epoch_count > self.epochs:
+            return
+        self.new_params = self.weight_maniputlator.apply_grad(grad, copy.deepcopy(self.prev_params))
+        self.model.set_weights(self.new_params)
+        self.new_loss = self.get_loss()
+        self.epoch_count += 1
+        if self.new_loss < self.prev_loss:
+            # model improved
+            new_grad = self.weight_maniputlator.calculate_gradient(copy.deepcopy(self.prev_params), copy.deepcopy(self.new_params), negate=False)
+
+            self.prev_params = self.new_params
+            self.prev_loss = self.new_loss
+
+            self.step_update(new_grad)
+        else:
+            new_grad = self.weight_maniputlator.calculate_gradient(copy.deepcopy(self.prev_params), copy.deepcopy(self.new_params), negate=True)
+
+            self.prev_params = self.new_params
+            self.prev_loss = self.new_loss
+
+            self.step_update(new_grad)
+        return
+
+
     def train(self, epochs=0):
+        self.model.build(input_shape=(None, config["MAX_SEQUENCE_LENGTH"]))
+        si = []
+        si = self.model.get_weights()
+        print("params")
+        print(si)
+        y = self.weight_maniputlator.get_params_array(si.copy())
+        y = self.weight_maniputlator.get_params_array(si.copy())
+        print("HHHHHH")
+        print(y)
+        old_grad = self.weight_maniputlator.generate_random_grad()
+        
+        f = self.weight_maniputlator.apply_grad(old_grad, copy.deepcopy(si))
+        #si = self.model.get_weights()
+        calc_grad = self.weight_maniputlator.calculate_gradient(si, f, negate=False)
+        
+        #print("grad")
+        #print(d)
+        print("old")
+        print(old_grad)
+        print("new")
+        print(calc_grad)
+        return
+        self.epoch_count = 0
+        self.epochs = epochs
+        self.prev_params = self.model.get_weights()
+        self.prev_loss = self.get_loss()
+
+        grad = self.weight_maniputlator.generate_random_grad()
+
+        self.step_update(grad)
+        
+
+        return
         self.prev_params = self.model.get_weights()
         self.prev_loss = self.get_loss()
 
@@ -112,9 +177,9 @@ class optimizer:
         self.new_loss = self.get_loss()
 
         if self.new_loss < self.prev_loss:
-            grad = self.calculate_gradient(self.prev_params, self.new_params, negate=False)
+            grad = self.weight_maniputlator.calculate_gradient(self.prev_params, self.new_params, negate=False)
         else:
-            grad = self.calculate_gradient(self.prev_params, self.new_params, negate=True)
+            grad = self.weight_maniputlator.calculate_gradient(self.prev_params, self.new_params, negate=True)
         
 
 
