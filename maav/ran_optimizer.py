3e164bf1a0c26333260109160cbff85310f22fde
ArchBase <archbase557@gmail.com>
2024-04-19 11:51:44 +0530

    added random grad changing algorithm
    added methods to change grad in case of oscillations around local minima

--- maav/ran_optimizer.py
+++ maav/ran_optimizer.py
@@ -3,13 +3,49 @@ from maav.configuration import config
 import maav.configuration as configuration
 import tensorflow as tf
 import copy
+from collections import Counter
 
 class History:
+    """
+    Class to mimic tensorflow's training history instance
+    """
     def __init__(self) -> None:
         self.history = {"loss":[], "val_loss":None}
+    def is_loss_oscillating(self, new_loss):
+        try:
+            last_5_loss_values = self.history["loss"][-3:]
+            element_counts = Counter(last_5_loss_values)
+            for loss, number_of_occurence in element_counts.items():
+                if number_of_occurence > 2:
+                    return True
+            return False
+        except IndexError:
+            return False
+        try:
+            avg = sum(self.history["loss"][-3:]) / 3
+            if not (avg - new_loss > 1):
+                return False
+            else:
+                return True
+        except IndexError:
+            return False
+
 
 
 class Weight_Manipulator:
+    """
+    This class is for manipulating weights of tensorflow model.
+    Weight_Manipulator is an abstract layer for handling tensorflow model weights
+
+    Methos:
+        generate_random_grad(): To create a random grad with respect to weights and biases
+        get_params_array(): Function to return a continous 1D array containing all weights of model
+            _format_params(): reccuresive method used by get_params_array()
+        apply_grad(): method to apply given grad to a given model parameters
+            _apply_grad_to_weights(): reccrsive method used by apply_grad()
+        calculate_gradient(): method to calculate grad given previous and new parameters
+
+    """
     def __init__(self) -> None:
         self.shape_reference = None
         self.weights_count = 0
@@ -39,20 +75,7 @@ class Weight_Manipulator:
         self._format_params(params)
         return self.line_params
 
-    def _ran_update_params(self, params):
-        self.weights_count += 1
-        if isinstance(params, np.ndarray) or type(params) is list:
-            for i in range(len(params)):
-                params[i] = self._ran_update_params(params[i])
-            return params
-        else:
-            params += np.random.uniform(config["MIN_UPDATE_FACTOR"], config["MAX_UPDATE_FACTOR"])
-            return params
-        
-    def random_update_params(self, params):
-        self.weights_count = 0
-        self.shape_reference = params
-        return self._ran_update_params(params)
+
     
     def _apply_grad_to_weights(self, params):
         if isinstance(params, np.ndarray) or type(params) is list:
@@ -94,6 +117,17 @@ class Weight_Manipulator:
             return self.grad.copy()
 
 class optimizer:
+    """
+    The main optimizer implemention class
+    This class uses ran optimization algorithm to optimize model weights to minimize loss function
+
+    Methods:
+        get_loss(): returns loss of model on train dataset
+        train(): method to train model, given number of epochs
+        step_update(): reccursive method used to train model using ran optimization algorithm
+
+
+    """
     def __init__(self, model, x_train, y_train, epochs) -> None:
         self.model = model
         self.epochs = epochs
@@ -127,15 +161,16 @@ class optimizer:
         self.new_loss = self.get_loss()
         self.history.history["loss"].append(self.new_loss)
         self.epoch_count += 1
+        
         if self.new_loss < self.prev_loss:
             # model improved
             new_grad = self.weight_maniputlator.calculate_gradient(copy.deepcopy(self.prev_params), copy.deepcopy(self.new_params), negate=False)
-
             self.prev_params = self.new_params
             self.prev_loss = self.new_loss
 
             self.step_update(new_grad)
         else:
+            # model not improved (negate gradient)
             new_grad = self.weight_maniputlator.calculate_gradient(copy.deepcopy(self.prev_params), copy.deepcopy(self.new_params), negate=True)
 
             self.prev_params = self.new_params
@@ -154,7 +189,7 @@ class optimizer:
         self.prev_loss = self.get_loss()
         self.history.history["loss"].append(self.prev_loss)
 
-        waste = self.weight_maniputlator.get_params_array(copy.deepcopy(self.prev_params))
+        waste = self.weight_maniputlator.get_params_array(copy.deepcopy(self.prev_params))# called to give self.weight_manipulator a reference of the model parameter array structure
         grad = self.weight_maniputlator.generate_random_grad()
         #print("grad")
         #print(grad)
@@ -162,49 +197,4 @@ class optimizer:
         
 
         return self.history
-        self.prev_params = self.model.get_weights()
-        self.prev_loss = self.get_loss()
-
-        self.new_params = self.weight_maniputlator.random_update_params(self.prev_params)
-        self.model.set_weights(self.new_params)
-        self.new_loss = self.get_loss()
-
-        if self.new_loss < self.prev_loss:
-            grad = self.weight_maniputlator.calculate_gradient(self.prev_params, self.new_params, negate=False)
-        else:
-            grad = self.weight_maniputlator.calculate_gradient(self.prev_params, self.new_params, negate=True)
         
-
-
-
-        return
-        self.model.build(input_shape=(None, config["MAX_SEQUENCE_LENGTH"]))
-        #for epoch in range(epochs):
-        #configuration.progress_bar("Training ran", epoch, epochs)
-        #self.prev_params = self.model.get_weights()
-        print(self.prev_params)
-        p = self.get_loss()
-        print("\nold\n")
-        print(p)
-        print("\n\n\nnew\n\n\n")
-        s = self.weight_maniputlator.random_update_params(self.model.get_weights())
-        #print(s)
-        self.model.set_weights(s)
-        print(self.get_loss())
-        #self.prev_loss, 
-
-
-        return
-        self.model.build(input_shape=(None, config["MAX_SEQUENCE_LENGTH"]))
-        for epoch in range(epochs):
-            configuration.progress_bar("training ran", epoch, epochs)
-        prev_params = self.model.get_weights()
-        print(prev_params)
-        self.random_update_params(prev_params)
-        print("\n\n\n\nnew modifiedn\n\n\n\n")
-        print(prev_params)
-
-        #perturbation = np.random.normal(loc=0, scale=0.01, size=prev_params.shape)
-        #modified_weights = prev_params + perturbation
-        #print(prev_params)
-
--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-id, survived, a, b, c
+"""id, survived, a, b, c
 1,0,3,22,1,0
 2,1,1,38,1,0
 3,1,3,26,0,0
 4,1,1,35,1,0
 5,0,3,35,0,0
+"""
+
+from collections import Counter
+
+my_list = [1, 2, 3, 4, 1, 2, 1, 1, 5]
+
+element_counts = Counter(my_list)
+
+print(element_counts[3])
